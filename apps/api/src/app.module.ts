a6b2e80d5606d83a3fe87ef54078935d8ec94055
Mohammad Aakib Bhat <bhataakib02@gmail.com>
2026-02-28 12:50:41 +0530

    cc

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -19,6 +20,9 @@ import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ThrottlerModule.forRoot([{
       ttl: 60000,
       limit: 100,
